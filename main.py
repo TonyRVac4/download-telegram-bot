bb7da676aae22d393dbca828b084024fd3104f90
Tony <Technicalprogram4658@gmail.com>
2022-06-15 17:04:16 +0300

    Add download_youtube_audio function
    Downloads a audio file from youtube.
    If an error occurs, it returns to the download_from_youtube function.
    Also the button handler: "Вернуться в главное меню"

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ def download_from_youtube(message):
         bot.register_next_step_handler(message, download_youtube_video)
     elif text == "Скачать Аудио":
         bot.send_message(chat_id, text="Введите URL:")
+        bot.register_next_step_handler(message, download_youtube_audio)
     elif text == "Вернуться в главное меню":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         button1 = types.KeyboardButton("Скачать из YouTube")
@@ -80,6 +81,34 @@ def download_youtube_video(message) -> None:
             filters = yt_obj.streams.filter(progressive=True, file_extension='mp4')
             filters.get_highest_resolution().download()
             bot.send_message(chat_id, text="Видео успешно загруженно")
+        except Exception:
+            bot.send_message(chat_id, text="Ошибка при скачивании!")
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            button1 = types.KeyboardButton("Скачать Видео")
+            button2 = types.KeyboardButton("Скачать Аудио")
+            back = types.KeyboardButton("Вернуться в главное меню")
+            markup.add(button1, button2, back)
+            bot.send_message(chat_id, text="Выберите вариант скачивания?", reply_markup=markup)
+            bot.register_next_step_handler(message, download_from_youtube)
+
+
+def download_youtube_audio(message) -> None:
+    chat_id = message.chat.id
+    text = message.text
+
+    if text == "Вернуться в главное меню":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button1 = types.KeyboardButton("Скачать из YouTube")
+        button2 = types.KeyboardButton("Скачать из Instagram")
+        markup.add(button1, button2)
+        bot.send_message(chat_id, text="Вы вернулись в главное меню", reply_markup=markup)
+    else:
+        try:
+            yt_obj = YouTube(message.text)
+            print(yt_obj.title)
+            file_name = "{} - Audio.mp4".format(yt_obj.title)
+            yt_obj.streams.get_audio_only().download(output_path='/Users/Tony/Downloads', filename=file_name)
+            bot.send_message(chat_id, text="Аудио файл успешно загружен")
         except Exception:
             bot.send_message(chat_id, text="Ошибка при скачивании!")
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
