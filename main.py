24c85f185f35a49cd0e9ed3a29d8678b8d40a26b
Tony <technicalprogram4658@gmail.com>
2022-11-20 21:25:30 +0300

    Fix main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from loader import bot
 import handlers
 from utils.set_bot_commands import set_default_commands
-from database.models import History
+from database.models import History, db
 
 
 set_default_commands()
 History.create_table()
+db.connect()
 bot.polling(none_stop=True, interval=0)
