c8b27b914f6d5263a0603a2077c4a51440bbc5a7
Tony <technicalprogram4658@gmail.com>
2022-11-21 12:41:32 +0300

    Fix main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from loader import bot
 import handlers
+import os
 from utils.set_bot_commands import set_default_commands
 
 set_default_commands()
+if str("files") not in os.listdir("TonyRVac4/download-telegram-bot/database/"):
+    os.mkdir("TonyRVac4/download-telegram-bot/database/files")
 bot.polling(none_stop=True, interval=0)
